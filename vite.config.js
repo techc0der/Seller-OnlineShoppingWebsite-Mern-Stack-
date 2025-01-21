6d2ee23ad3b2f08510e91e1c74a84db50e7f95eb
Sandip <sandipsuthar086@gmail.com>
2025-01-21 23:27:53 +0530

    seller is added

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: parseInt(process.env.PORT) || 5173, // Use the PORT variable if available
+    host: true, // Allow access over the network
+  },
 })
